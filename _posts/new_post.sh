a9f43214111e8373e9a5002c61eaf6e9b043c9d7
Lee Junyeol <shie44167@gmail.com>
2024-06-02 16:23:22 +0900

    fix content

--- _posts/new_post.sh
+++ _posts/new_post.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+title=$1
 date=$(date '+%Y-%m-%d')
 cur_date_and_time=$(date '+%Y-%m-%d %H:%M:%S')
 
-touch ${date}-TITLE.md
+touch ${date}-${title}.md
 
-cat <<EOF >${date}-TITLE.md
+cat <<EOF >${date}-${title}.md
 ---
 title: TITLE
 date: ${cur_date_and_time}
