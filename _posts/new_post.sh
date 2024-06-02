24245fccc02ead4d592a7035be760822a1fef1d9
Lee Junyeol <shie44167@gmail.com>
2024-06-02 16:31:17 +0900

    2024-06-02 fix content

--- _posts/new_post.sh
+++ _posts/new_post.sh
@@ -9,7 +9,7 @@ touch ${date}-${title}.md
 cat <<EOF >${date}-${title}.md
 ---
 title: TITLE
-date: ${cur_date_and_time}
+date: ${cur_date_and_time} +0900
 categories: [TOP_CATEGORIE, SUB_CATEGORIE]
 tags: [TAG]     # TAG names should always be lowercase
 toc: true
