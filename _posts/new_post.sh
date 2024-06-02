ab8e0ee881ec71151c822148b9d77951d63f66ab
Lee Junyeol <shie44167@gmail.com>
2024-06-02 16:14:32 +0900

    2024-06-02 fix content name

--- _posts/new_post.sh
+++ _posts/new_post.sh
@@ -3,13 +3,14 @@
 date=$(date '+%Y-%m-%d')
 cur_date_and_time=$(date '+%Y-%m-%d %H:%M:%S')
 
-touch ${date}.md
+touch ${date}-TITLE.md
 
-cat <<EOF >${date}.md
+cat <<EOF >${date}-TITLE.md
 ---
 title: TITLE
 date: ${cur_date_and_time}
 categories: [TOP_CATEGORIE, SUB_CATEGORIE]
 tags: [TAG]     # TAG names should always be lowercase
+toc: true
 ---
 EOF
